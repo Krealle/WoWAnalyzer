3a585a49c1e2489dbd6b868eab31e78be063202e
krealle <vollmer@archon.gg>
2026-03-23 20:19:41 +0100

    regen talents to get updated mana costs

--- scripts/talents/generate-talents.ts
+++ scripts/talents/generate-talents.ts
@@ -22,7 +22,7 @@ import {
   TalentNode,
 } from './talent-tree-types';
 
-const LIVE_WOW_BUILD_NUMBER = '12.0.1.66220';
+const LIVE_WOW_BUILD_NUMBER = '12.0.1.66527';
 const LIVE_TALENT_DATA_URL = `https://www.raidbots.com/static/data/${LIVE_WOW_BUILD_NUMBER}/talents.json`;
 const LIVE_SPELLPOWER_DATA_URL = `https://wago.tools/db2/SpellPower/csv?build=${LIVE_WOW_BUILD_NUMBER}`;
 const PTR_WOW_BUILD_NUMBER = '12.0.1.66220';
